6f88b94bcf2b2ef913fda4a0d9572e572df4e4c1
Daniel Fischer <daniel-fischer@hotmail.de>
2022-02-13 21:22:01 +0100

    Remove debug output

--- raidTimeline.App/Program.cs
+++ raidTimeline.App/Program.cs
@@ -10,8 +10,6 @@ using raidTimeline.Logic.Interfaces;
 
 var builder = CoconaApp.CreateBuilder();
 
-Console.WriteLine(Directory.GetCurrentDirectory());
-
 builder.Configuration.AddJsonFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.json"), 
     false, true);
 builder.Services.AddSingleton<ConfigurationHelper>();
