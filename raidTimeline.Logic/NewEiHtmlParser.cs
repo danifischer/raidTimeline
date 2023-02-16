4d2545f181f5840fe203a3b232aa48accaca5a33
Daniel Fischer <daniel-fischer@hotmail.de>
2023-02-16 21:59:57 +0100

    Fix bug, test change in parser

---
+++ raidTimeline.Logic/NewEiHtmlParser.cs
@@ -0,0 +1,192 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+using raidTimeline.Logic.Mechanics;
+using raidTimeline.Logic.Models;
+using Newtonsoft.Json.Linq;
+
+[assembly: InternalsVisibleTo("raidTimeline.Logic.Tests")]
+
+namespace raidTimeline.Logic
+{
+    internal static class NewEiHtmlParser
+    {
+        private static readonly MechanicsFactory MechanicsFactory = MechanicsFactory.GetMechanicsFactory(); 
+
+        internal static RaidModel ParseLog(string filePath, ILogger logger = null)
+        {
+            var model = new RaidModel(filePath);
+
+            try
+            {
+                var logData = GetLogData(File.ReadAllText(model.LogPath));
+
+                SetRemainingHealth(model, logData);
+                SetGeneralInformation(model, logData);
+                if (logData["eiVersion"] != null || logData["parser"] != null)
+                {
+                    model.OccurenceStart = logData["encounterStart"].Value<DateTime>();
+                    model.OccurenceEnd = logData["encounterEnd"].Value<DateTime>();
+                }
+
+                var fightDuration = logData["phases"][0]["duration"].Value<long>();
+
+                for (var j = 0; j < logData["players"].Count(); j++)
+                {
+                    var player = logData["players"][j];
+                    var playerModel = new PlayerModel
+                    {
+                        Index = j,
+                        AccountName = player["acc"].Value<string>(),
+                        Profession = player["profession"].Value<string>(),
+                        ProfessionIcon = player["icon"].Value<string>(),
+                        IsNpc = player["notInSquad"].Value<bool>(),
+                        Group = player["group"].Value<int>()
+                    };
+
+                    ParseSupportStats(playerModel, logData);
+                    ParseDamageStats(playerModel, fightDuration, logData);
+                    //ParseMechanics(playerModel, model, logData, logger);
+                    ParseBoons(playerModel, logData);
+
+                    model.Players.Add(playerModel);
+                }
+            }
+            catch (Exception e)
+            {
+                logger?.LogError($">>> {filePath} cannot be parsed.");
+                logger?.LogDebug(e.Message);
+                return null;
+            }
+
+            return model;
+        }
+
+        private static void ParseBoons(this PlayerModel playerModel, JObject logData)
+        {
+            var boons = logData["boons"];
+            var boonList = new List<string>();
+
+            foreach (var boon in boons)
+            {
+                boonList.Add(boon.Value<string>());
+            }
+
+            var maps = new List<BuffModel>();
+            var map = logData["buffMap"];
+
+            foreach (var mappedBoon in map)
+            {
+                var index = boonList.IndexOf(mappedBoon.First["id"].Value<string>());
+
+                if (index != -1)
+                {
+                    maps.Add(new BuffModel
+                    { 
+                        Icon = mappedBoon.First["icon"].Value<string>(),
+                        Name = mappedBoon.First["name"].Value<string>(),
+                        Stacking = mappedBoon.First["stacking"].Value<bool>(),
+                        Id = mappedBoon.First["id"].Value<string>(),
+                        Index = index,
+                    });
+                }
+            }
+
+            var buffs = logData["phases"][0]["boonStats"];
+            maps = maps.OrderBy(i => i.Index).ToList();
+
+            for (var i = 0; i < maps.Count; i++)
+            {
+                var x = buffs[playerModel.Index]["data"][i];
+
+                switch (x.Count())
+                {
+                    case 1:
+                        maps[i].Value = x[0].Value<double>();
+                        maps[i].Stacks = null;
+                        break;
+                    case 2:
+                        maps[i].Value = x[0].Value<double>();
+                        maps[i].Stacks = x[1].Value<double>();
+                        break;
+                    default:
+                        maps[i].Value = 0;
+                        maps[i].Stacks = null;
+                        break;
+                }
+            }
+
+            playerModel.Buffs.AddRange(maps);
+        }
+
+        private static void ParseMechanics(this PlayerModel playerModel, RaidModel raidModel, JObject logData,
+            ILogger logger = null)
+        {
+            var strategy = MechanicsFactory.FindStrategy(raidModel.EncounterIcon);
+            try
+            {
+                strategy?.Parse(logData, playerModel);
+            }
+            catch (Exception)
+            {
+                logger?.LogDebug(">>> Mechanics not parsable: " + raidModel.LogUrl);
+            }
+        }
+
+        private static void ParseDamageStats(this PlayerModel playerModel, long fightDuration, JObject logData)
+        {
+            var targets = logData["players"][playerModel.Index]["details"]["dmgDistributionsTargets"][0];
+            playerModel.Damage = 0;
+
+            for (var i = 0; i < logData["phases"][0]["targets"].Count(); i++)
+            {
+                playerModel.Damage += targets[i]["totalDamage"].Value<long>();
+            }
+
+            playerModel.Dps = playerModel.Damage * 1000 / fightDuration;
+        }
+
+        private static void ParseSupportStats(this PlayerModel playerModel, JObject logData)
+        {
+            var supportStats = logData["phases"][0]["supportStats"][playerModel.Index];
+
+            playerModel.ResAmount = supportStats[6].Value<int>();
+            playerModel.ResTime = supportStats[7].Value<double>();
+            playerModel.Cc = logData["players"][playerModel.Index]["details"]["dmgDistributions"][0]["totalBreakbarDamage"].Value<long>();
+        }
+
+        private static void SetGeneralInformation(this RaidModel raidModel, JObject logData)
+        {
+            raidModel.EncounterTime = logData["encounterDuration"].Value<string>();
+            raidModel.EncounterIcon = logData["fightIcon"].Value<string>();
+            raidModel.EncounterName = logData["fightName"].Value<string>();
+            if (logData["fightID"] != null)
+            {
+                raidModel.EncounterId = logData["fightID"].Value<int>();
+            }
+            raidModel.Killed = logData["success"].Value<bool>();
+        }
+
+        private static void SetRemainingHealth(this RaidModel raidModel, JObject logData)
+        {
+            for (var i = 0; i < logData["phases"][0]["targets"].Count(); i++)
+            {
+                var targetData = logData["targets"][logData["phases"][0]["targets"][i].Value<int>()];
+                raidModel.HpLeft.Add(targetData["hpLeft"].Value<double>());
+            }
+        }
+
+        private static JObject GetLogData(string encounter)
+        {
+            var indexStart = encounter.IndexOf("var _logData = ", StringComparison.Ordinal);
+            var indexEnd = encounter.IndexOf("]};", indexStart, StringComparison.Ordinal) + 2;
+            var json = encounter[(indexStart + 15)..indexEnd];
+            return JObject.Parse(json);
+        }
+    }
+}
--- raidTimeline.Logic/TimelineCreator.cs
+++ raidTimeline.Logic/TimelineCreator.cs
@@ -1,258 +1,254 @@
-﻿using Newtonsoft.Json;
-using RestSharp;
+﻿using Microsoft.Extensions.Logging;
+using raidTimeline.Logic.DpsReport;
+using raidTimeline.Logic.Interfaces;
+using raidTimeline.Logic.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using raidTimeline.Logic.DpsReport;
-using raidTimeline.Logic.Interfaces;
-using raidTimeline.Logic.Models;
 
 namespace raidTimeline.Logic
 {
-	public class TimelineCreator : ITimelineCreator
-	{
-		private readonly ILogger _logger;
+    public class TimelineCreator : ITimelineCreator
+    {
+        private readonly ILogger _logger;
 
-		public TimelineCreator(ILogger<TimelineCreator> logger = null)
-		{
-			_logger = logger;
-		}
+        public TimelineCreator(ILogger<TimelineCreator> logger = null)
+        {
+            _logger = logger;
+        }
 
-		/// <summary>
-		/// Parse all elite insights html files in a defined path.
-		/// </summary>
-		/// <param name="path">Path which is used to search for html files in.</param>
-		/// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
-		/// <returns>List of parsed raid models.</returns>
-		public IList<RaidModel> ParseFilesFromDisk(string path, CancellationToken cancellationToken)
-		{
-			var models = new List<RaidModel>();
-			
-			var parallelOptions = new ParallelOptions
-			{
-				CancellationToken = cancellationToken,
-				MaxDegreeOfParallelism = 5
-			};
+        /// <summary>
+        /// Parse all elite insights html files in a defined path.
+        /// </summary>
+        /// <param name="path">Path which is used to search for html files in.</param>
+        /// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
+        /// <returns>List of parsed raid models.</returns>
+        public IList<RaidModel> ParseFilesFromDisk(string path, CancellationToken cancellationToken)
+        {
+            var models = new List<RaidModel>();
 
-			Parallel.ForEach(Directory.GetFiles(path, "*.html"), parallelOptions, 
-				filePath =>
-			{
-				_logger?.LogTrace($"Parsing log: {Path.GetFileName(filePath)}");
-				var model = EiHtmlParser.ParseLog(filePath);
-				if (model != null)
-					models.Add(model);
-			});
-			
-			return models;
-		}
+            var parallelOptions = new ParallelOptions
+            {
+                CancellationToken = cancellationToken,
+                MaxDegreeOfParallelism = 5
+            };
 
-		/// <summary>
-		/// Parse all elite insights html files in a defined path, but ignores already parsed ones.
-		/// </summary>
-		/// <param name="path">Path which is used to search for html files in.</param>
-		/// <param name="outputFileName">File name if the summary html file.</param>
-		/// <param name="models">List of already parsed raid models.</param>
-		/// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
-		/// <returns>List of parsed raid models.</returns>
-		public IList<RaidModel> ParseFilesFromDiskWhileWatching(string path, string outputFileName, 
-			IList<RaidModel> models, CancellationToken cancellationToken = new())
-		{
-			var parallelOptions = new ParallelOptions
-			{
-				CancellationToken = cancellationToken,
-				MaxDegreeOfParallelism = 5
-			};
-			
-			var knownFiles = models.Select(i => i.LogPath).ToArray();
+            Parallel.ForEach(Directory.GetFiles(path, "*.html"), parallelOptions,
+                filePath =>
+            {
+                _logger?.LogTrace($"Parsing log: {Path.GetFileName(filePath)}");
+                var model = EiHtmlParser.ParseLog(filePath);
+                if (model != null)
+                    models.Add(model);
+            });
 
-			Parallel.ForEach(Directory.GetFiles(path, "*.html"), parallelOptions, 
-				filePath =>
-			{
-				if (knownFiles.Contains(filePath)) return;
-				if (filePath.EndsWith(outputFileName)) return;
+            return models;
+        }
 
-				_logger?.LogTrace($"Parsing log: {Path.GetFileName(filePath)} ");
-				var model = EiHtmlParser.ParseLog(filePath, _logger);
-				if (model != null)
-				{
-					_logger?.LogTrace($"Finished: {Path.GetFileName(filePath)}");
-					models.Add(model);
-				}
-			});
+        /// <summary>
+        /// Parse all elite insights html files in a defined path, but ignores already parsed ones.
+        /// </summary>
+        /// <param name="path">Path which is used to search for html files in.</param>
+        /// <param name="outputFileName">File name if the summary html file.</param>
+        /// <param name="models">List of already parsed raid models.</param>
+        /// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
+        /// <returns>List of parsed raid models.</returns>
+        public IList<RaidModel> ParseFilesFromDiskWhileWatching(string path, string outputFileName,
+            IList<RaidModel> models, CancellationToken cancellationToken = new())
+        {
+            var parallelOptions = new ParallelOptions
+            {
+                CancellationToken = cancellationToken,
+                MaxDegreeOfParallelism = 5
+            };
 
-			return models;
-		}
+            var knownFiles = models.Select(i => i.LogPath).ToArray();
 
-		/// <summary>
-		/// Creates a summary html file from the parsed raid models.
-		/// </summary>
-		/// <param name="path">The path where the file should be saved.</param>
-		/// <param name="outputFileName">The filename which shall be used.</param>
-		/// <param name="models">List of parsed raid models.</param>
-		/// <param name="reverse">If 'true' the order is from newest to oldest, otherwise from oldest to newest.
-		/// Default value is 'false'.</param>
-		/// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
-		public void BuildTimelineFile(string path, string outputFileName, IEnumerable<RaidModel> models, 
-			bool reverse = false, CancellationToken cancellationToken = new())
-		{
-			var htmlFileName = outputFileName;
-			string htmlFilePath = Path.Combine(path, htmlFileName);
+            Parallel.ForEach(Directory.GetFiles(path, "*.html"), parallelOptions,
+                filePath =>
+            {
+                if (knownFiles.Contains(filePath)) return;
+                if (filePath.EndsWith(outputFileName)) return;
 
-			if (File.Exists(htmlFilePath))
-			{
-				File.Delete(Path.Combine(htmlFilePath));
-			}
+                _logger?.LogTrace($"Parsing log: {Path.GetFileName(filePath)} ");
+                var model = EiHtmlParser.ParseLog(filePath, _logger);
+                if (model != null)
+                {
+                    _logger?.LogTrace($"Finished: {Path.GetFileName(filePath)}");
+                    models.Add(model);
+                }
+            });
 
-			var sb = new StringBuilder();
+            return models;
+        }
 
-			var ordered = reverse
-				? models.OrderByDescending(i => i.OccurenceStart)
-				: models.OrderBy(i => i.OccurenceStart);
+        /// <summary>
+        /// Creates a summary html file from the parsed raid models.
+        /// </summary>
+        /// <param name="path">The path where the file should be saved.</param>
+        /// <param name="outputFileName">The filename which shall be used.</param>
+        /// <param name="models">List of parsed raid models.</param>
+        /// <param name="reverse">If 'true' the order is from newest to oldest, otherwise from oldest to newest.
+        /// Default value is 'false'.</param>
+        /// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
+        public void BuildTimelineFile(string path, string outputFileName, IEnumerable<RaidModel> models,
+            bool reverse = false, CancellationToken cancellationToken = new())
+        {
+            var htmlFileName = outputFileName;
+            string htmlFilePath = Path.Combine(path, htmlFileName);
 
-			foreach (var raidData in ordered.GroupBy(i => i.OccurenceStart.Date))
-			{		
-				CreateHeader(sb, raidData);
+            if (File.Exists(htmlFilePath))
+            {
+                File.Delete(Path.Combine(htmlFilePath));
+            }
 
-				sb.CreateTimelineHeader();
-				CreateTimeline(sb, raidData, reverse);
-				sb.CreateTimelineFooter();
+            var sb = new StringBuilder();
 
-				if (cancellationToken.IsCancellationRequested) return;
-			}
+            var ordered = reverse
+                ? models.OrderByDescending(i => i.OccurenceStart)
+                : models.OrderBy(i => i.OccurenceStart);
 
-            BuildStatisticsFile(path, models, reverse, cancellationToken);	
+            foreach (var raidData in ordered.GroupBy(i => i.OccurenceStart.Date))
+            {
+                CreateHeader(sb, raidData);
 
-			if (cancellationToken.IsCancellationRequested) return;
-			WriteHtmlFile(htmlFileName, htmlFilePath, sb, @"templateTimeline.html");
-			_logger?.LogTrace("HTML Magic >>> Done");
-		}
+                sb.CreateTimelineHeader();
+                CreateTimeline(sb, raidData, reverse);
+                sb.CreateTimelineFooter();
 
-		/// <summary>
-		/// Parse elite insights html files from dps.report.
-		/// </summary>
-		/// <param name="path">The path which shall be used to temporarily store logs.</param>
-		/// <param name="token">The dps.report token for the user.</param>
-		/// <param name="day">The day that shall be parsed.</param>
-		/// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
-		/// <returns>List of parsed raid models.</returns>
-		public IList<RaidModel> ParseFileFromWeb(string path, string token, string day, 
-			CancellationToken cancellationToken = new())
-		{
-			var downloader = new LogDownloader();
-			return downloader.DownloadLogsForOneDay(path, token, day, cancellationToken);
-		}
+                if (cancellationToken.IsCancellationRequested) return;
+            }
 
-		private static void BuildStatisticsFile(string path, IEnumerable<RaidModel> models,
-			bool reverse, CancellationToken cancellationToken)
+            BuildStatisticsFile(path, models, reverse, cancellationToken);
+
+            if (cancellationToken.IsCancellationRequested) return;
+            WriteHtmlFile(htmlFileName, htmlFilePath, sb, @"templateTimeline.html");
+            _logger?.LogTrace("HTML Magic >>> Done");
+        }
+
+        /// <summary>
+        /// Parse elite insights html files from dps.report.
+        /// </summary>
+        /// <param name="path">The path which shall be used to temporarily store logs.</param>
+        /// <param name="token">The dps.report token for the user.</param>
+        /// <param name="day">The day that shall be parsed.</param>
+        /// <param name="cancellationToken">Cancellation token for stopping the parsing.</param>
+        /// <returns>List of parsed raid models.</returns>
+        public IList<RaidModel> ParseFileFromWeb(string path, string token, string day,
+            CancellationToken cancellationToken = new())
+        {
+            var downloader = new LogDownloader();
+            return downloader.DownloadLogsForOneDay(path, token, day, cancellationToken);
+        }
+
+        private static void BuildStatisticsFile(string path, IEnumerable<RaidModel> models,
+            bool reverse, CancellationToken cancellationToken)
         {
             const string htmlFileName = "statistics.html";
-			string htmlFilePath = Path.Combine(path, htmlFileName);
+            string htmlFilePath = Path.Combine(path, htmlFileName);
 
-			if (File.Exists(htmlFilePath))
-			{
-				File.Delete(Path.Combine(htmlFilePath));
-			}
+            if (File.Exists(htmlFilePath))
+            {
+                File.Delete(Path.Combine(htmlFilePath));
+            }
 
-			var sb = new StringBuilder();
+            var sb = new StringBuilder();
 
-			var ordered = reverse
-				? models.OrderByDescending(i => i.OccurenceStart)
-				: models.OrderBy(i => i.OccurenceStart);
+            var ordered = reverse
+                ? models.OrderByDescending(i => i.OccurenceStart)
+                : models.OrderBy(i => i.OccurenceStart);
 
-			foreach (var raidData in ordered.GroupBy(i => i.OccurenceStart.Date))
-			{
+            foreach (var raidData in ordered.GroupBy(i => i.OccurenceStart.Date))
+            {
+                CreateHeader(sb, raidData);
+                CreateProfessionTable(sb, raidData, reverse);
+                CreateEncounterTable(sb, raidData, reverse);
 
-				CreateHeader(sb, raidData);
-				CreateProfessionTable(sb, raidData, reverse);
-				CreateEncounterTable(sb, raidData, reverse);
+                if (cancellationToken.IsCancellationRequested) return;
+            }
 
-				if (cancellationToken.IsCancellationRequested) return;
-			}
+            if (cancellationToken.IsCancellationRequested) return;
+            WriteHtmlFile(htmlFileName, htmlFilePath, sb, @"templateStatistics.html");
+        }
 
-			if (cancellationToken.IsCancellationRequested) return;
-			WriteHtmlFile(htmlFileName, htmlFilePath, sb, @"templateStatistics.html");
-		}
+        private static void WriteHtmlFile(string htmlFileName, string htmlFilePath, StringBuilder sb, string template)
+        {
+            var html = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, template));
+            html = html.Replace("{{placeholder}}", sb.ToString());
+            File.WriteAllText(htmlFileName, html);
+            File.Move(htmlFileName, htmlFilePath);
+        }
 
-		private static void WriteHtmlFile(string htmlFileName, string htmlFilePath, StringBuilder sb, string template)
-		{
-			var html = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, template));
-			html = html.Replace("{{placeholder}}", sb.ToString());
-			File.WriteAllText(htmlFileName, html);
-			File.Move(htmlFileName, htmlFilePath);
-		}
+        private static void CreateTimeline(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData, bool reverse)
+        {
+            var ordered = reverse
+                ? raidData.OrderByDescending(i => i.OccurenceEnd)
+                : raidData.OrderBy(i => i.OccurenceEnd);
 
-		private static void CreateTimeline(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData, bool reverse)
-		{
-			var ordered = reverse
-				? raidData.OrderByDescending(i => i.OccurenceEnd)
-				: raidData.OrderBy(i => i.OccurenceEnd);
+            foreach (var model in ordered)
+            {
+                sb.Append(model.Killed
+                    ? HtmlCreator.CreateEncounterHtmlPass(model)
+                    : HtmlCreator.CreateEncounterHtmlFail(model));
+            }
+        }
 
-			foreach (var model in ordered)
-			{
-				sb.Append(model.Killed
-					? HtmlCreator.CreateEncounterHtmlPass(model)
-					: HtmlCreator.CreateEncounterHtmlFail(model));
-			}
-		}
+        private static void CreateEncounterTable(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData, bool reverse)
+        {
+            var ordered = reverse
+                ? raidData.OrderByDescending(i => i.OccurenceEnd)
+                : raidData.OrderBy(i => i.OccurenceEnd);
 
-		private static void CreateEncounterTable(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData, bool reverse)
-		{
-			var ordered = reverse
-				? raidData.OrderByDescending(i => i.OccurenceEnd)
-				: raidData.OrderBy(i => i.OccurenceEnd);
+            var players = ordered.SelectMany(i => i.Players).Where(j => !j.IsNpc)
+                .Select(j => j.AccountName)
+                .Distinct().OrderBy(name => name).ToArray();
 
-			var players = ordered.SelectMany(i => i.Players).Where(j => !j.IsNpc)
-				.Select(j => j.AccountName)
-				.Distinct().OrderBy(name => name).ToArray();
+            sb.Append(HtmlCreator.CreateEncounterTableHeader(players));
 
-			sb.Append(HtmlCreator.CreateEncounterTableHeader(players));
+            foreach (var model in ordered)
+            {
+                var tableRow = new EncounterTableModel(model, players);
+                sb.Append(HtmlCreator.CreateEncounterTableEntry(tableRow));
+            }
 
-			foreach (var model in ordered)
-			{
-				var tableRow = new EncounterTableModel(model, players);
-				sb.Append(HtmlCreator.CreateEncounterTableEntry(tableRow));
-			}
+            sb.Append(HtmlCreator.CreateEncounterTableFooter());
+        }
 
-			sb.Append(HtmlCreator.CreateEncounterTableFooter());
-		}
+        private static void CreateProfessionTable(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData, bool reverse)
+        {
+            var ordered = reverse
+                ? raidData.OrderByDescending(i => i.OccurenceEnd)
+                : raidData.OrderBy(i => i.OccurenceEnd);
 
-		private static void CreateProfessionTable(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData, bool reverse)
-		{
-			var ordered = reverse
-				? raidData.OrderByDescending(i => i.OccurenceEnd)
-				: raidData.OrderBy(i => i.OccurenceEnd);
+            var tryTime = new TimeSpan(raidData.Select(i => i.OccurenceEnd - i.OccurenceStart)
+                .Sum(i => i.Ticks));
 
-			var tryTime = new TimeSpan(raidData.Select(i => i.OccurenceEnd - i.OccurenceStart)
-				.Sum(i => i.Ticks));
+            sb.Append(HtmlCreator.CreateOverviewTableHeader());
 
-			sb.Append(HtmlCreator.CreateOverviewTableHeader());
+            foreach (var group in ordered.GroupBy(i => i.EncounterName))
+            {
+                var tableRow = new OverviewTableModel(group.ToArray(), tryTime);
+                sb.Append(HtmlCreator.CreateOverviewTableEntry(tableRow));
+            }
 
-			foreach (var group in ordered.GroupBy(i => i.EncounterName))
-			{
-				var tableRow = new OverviewTableModel(group.ToArray(), tryTime);
-				sb.Append(HtmlCreator.CreateOverviewTableEntry(tableRow));
-			}
+            sb.Append(HtmlCreator.CreateOverviewTableFooter());
+        }
 
-			sb.Append(HtmlCreator.CreateOverviewTableFooter());
-		}
+        private static void CreateHeader(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData)
+        {
+            var killed = raidData.Count(i => i.Killed);
+            var failed = raidData.Count(i => !i.Killed);
+            var bosses = raidData.Select(i => i.EncounterName).Distinct().Count();
 
-		private static void CreateHeader(StringBuilder sb, IGrouping<DateTime, RaidModel> raidData)
-		{
-			var killed = raidData.Count(i => i.Killed);
-			var failed = raidData.Count(i => !i.Killed);
-			var bosses = raidData.Select(i => i.EncounterName).Distinct().Count();
+            var tryTime = new TimeSpan(raidData.Select(i => i.OccurenceEnd - i.OccurenceStart)
+                .Sum(i => i.Ticks));
+            var raidTime = raidData.Max(i => i.OccurenceEnd) - raidData.Min(i => i.OccurenceStart);
 
-			var tryTime = new TimeSpan(raidData.Select(i => i.OccurenceEnd - i.OccurenceStart)
-				.Sum(i => i.Ticks));
-			var raidTime = raidData.Max(i => i.OccurenceEnd) - raidData.Min(i => i.OccurenceStart);
-
-			sb.Append(HtmlCreator.CreateHeaderHtml(raidData.Key, killed, failed, tryTime, raidTime, bosses));
-		}
-	}
+            sb.Append(HtmlCreator.CreateHeaderHtml(raidData.Key, killed, failed, tryTime, raidTime, bosses));
+        }
+    }
 }
